05b7b45a7f3e8a38eca4384dee3128de9c72d36c
David <davidtoman07@gmail.com>
2024-12-07 15:06:46 +1300

    zipline works

--- Assets/scripts/zipline.cs
+++ Assets/scripts/zipline.cs
@@ -7,6 +7,9 @@ public class zipline : MonoBehaviour
 {
     public GameObject left;
     public GameObject right;
+    private bool isMoving = false;
+    GameObject player;
+    RigidbodyConstraints bruh;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +20,36 @@ public class zipline : MonoBehaviour
     {
         if (collider.gameObject.name == "zipline collider" && collider.transform.parent.GetComponent<Rigidbody>().velocity.y < 0f)
         {
-            GameObject player = collider.transform.parent.gameObject;
+            player = collider.transform.parent.gameObject;
             Debug.Log("this feller is ziplining: " + player.GetComponent<playa>().playaNumber);
-            player.transform.DOMove(right.transform.position - new Vector3(0f, 1.6f), 2f);
+            player.GetComponent<Rigidbody>().useGravity = false;
+            // bruh = player.GetComponent<Rigidbody>().constraints;
+            player.GetComponent<Rigidbody>().constraints |= RigidbodyConstraints.FreezeRotationZ;
+
+            if (transform.position != right.transform.position - new Vector3(0f, 1.6f))
+            {
+                isMoving = true;
+            }
+
+
+            // player.transform.DOMove(right.transform.position - new Vector3(0f, 1.6f), 2f);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isMoving)
+        {
+            // Move the object toward the target position at a constant speed
+            player.transform.position = Vector3.MoveTowards(player.transform.position, right.transform.position - new Vector3(0f, 1.6f), 10f * Time.deltaTime);
 
+            if (Vector3.Distance(player.transform.position, right.transform.position - new Vector3(0f, 1.6f)) < 0.01f)
+            {
+                isMoving = false;
+                player.GetComponent<Rigidbody>().useGravity = true;
+                player.GetComponent<Rigidbody>().constraints &= ~RigidbodyConstraints.FreezeRotationZ;
+            }
+        }
     }
 }
